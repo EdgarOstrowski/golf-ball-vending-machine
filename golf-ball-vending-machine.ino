f16833da3fbe7f3c8a3f091ab01d3216586f5d47
Edgar Ostrowski <edgar.ostrowski@gmail.com>
2022-03-22 22:11:13 +0100

    Refactoring

--- golf-ball-vending-machine.ino
+++ golf-ball-vending-machine.ino
@@ -1,9 +1,8 @@
 
-/*
- * https://forum.arduino.cc/t/dc-motor-with-l298n-and-led-blinking-same-time/681187/3
- * 
- */
-const int pushButton = 2;
+const int coinOne = 2;
+const int coinTwo = 3;
+const int coinThree = 4;
+
 const int motorOne = 8;
 const int motorTwo = 9;
 const int vendingDuration = 5000;
@@ -15,83 +14,81 @@ enum statesVending
   UP
 };
 
+bool ledState;
 int vendingState;
+
 unsigned long timeNow;
 unsigned long timeVendingStart;
-unsigned long timeBlink;
-
-bool ledState;
-
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pushButton, INPUT);
-  pinMode(motorOne, OUTPUT);  
+  pinMode(coinOne, INPUT);
+  pinMode(coinTwo, INPUT);
+  pinMode(coinThree, INPUT);
+  pinMode(motorOne, OUTPUT);
   pinMode(motorTwo, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  vendingState = statesVending::NOT_RUNNING;
-
-  // ledState = false;
-  // digitalWrite(motorOne, HIGH);
-  // digitalWrite(motorTwo, HIGH);  
+  vendingState = statesVending::UP;
+  // NOT_RUNNING;
 }
 
-void loop() { 
-  statusLED();  
+void loop() {
   timeNow = millis();
-  int buttonState = digitalRead(pushButton);
-  
-  switch( vendingState )
+  updateStatusLED();
+
+  int buttonState = digitalRead(coinOne);
+
+  switch ( vendingState )
   {
-    case statesVending::NOT_RUNNING: 
-      
+    case statesVending::NOT_RUNNING:
+
       // Turn off motor
       digitalWrite(motorOne, HIGH);
       digitalWrite(motorTwo, HIGH);
       digitalWrite(LED_BUILTIN, LOW);
-        
-        if (buttonState == HIGH)
-        {
-          vendingState = statesVending::DOWN;
-          timeVendingStart = millis();                   
-        }
-    break;
+
+      if (buttonState == HIGH)
+      {
+        vendingState = statesVending::DOWN;
+        timeVendingStart = millis();
+      }
+      break;
 
     case statesVending::DOWN:
-        flashLED();
-        
-        // Turn on motor          
-        digitalWrite(motorOne, HIGH);
-        digitalWrite(motorTwo, LOW);        
-   
-        if (timeNow - timeVendingStart >= vendingDuration)
-        {
-          vendingState = statesVending::UP;
-          timeVendingStart = millis();
-        }
+      flashStatusLED();
 
-    break;
+      // Turn on motor
+      digitalWrite(motorOne, HIGH);
+      digitalWrite(motorTwo, LOW);
+
+      if (timeNow - timeVendingStart >= vendingDuration)
+      {
+        vendingState = statesVending::UP;
+        timeVendingStart = millis();
+      }
+
+      break;
 
     case statesVending::UP:
-        flashLED();
-        
-        // Reverse motor
-        digitalWrite(motorOne, LOW);
-        digitalWrite(motorTwo, HIGH);
-          
-        if (timeNow - timeVendingStart >= vendingDuration)
-        {
-          vendingState = statesVending::NOT_RUNNING;
-        }
+      flashStatusLED();
 
-    break;    
+      // Reverse motor
+      digitalWrite(motorOne, LOW);
+      digitalWrite(motorTwo, HIGH);
+
+      if (timeNow - timeVendingStart >= vendingDuration)
+      {
+        vendingState = statesVending::NOT_RUNNING;
+      }
+
+      break;
   }
 
 }
 
-void statusLED(void)
+void updateStatusLED(void)
 {
   static unsigned long timeBlink;
 
@@ -102,14 +99,14 @@ void statusLED(void)
   }
 }
 
-void flashLED(void)
+void flashStatusLED(void)
 {
   if (ledState == false)
   {
-  digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
   }
   else
   {
-  digitalWrite(LED_BUILTIN, LOW);            
-  }  
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
